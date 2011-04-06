224a6bc4984fa1d245556dfabcb807ef7ba8c030
shaun <shaun@shaun-Studio-1745.(none)>
2011-04-05 22:25:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3play::Application.config.secret_token = '5a4bb05d817bbc5d0ad18c416049c39f3f6b22108923cd63c4bb7828a3a900966e5883f49dcb1e2e4bc63b5ee98c086692fab046f066092465a269358cd7fffe'
